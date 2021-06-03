47db1655a0f94c34bb7701e212e91c5d2c9eabee
Jaime Cedillo <74879881+jaimecedillo@users.noreply.github.com>
2021-06-02 20:10:17 -0600

    test contact

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -7,6 +7,8 @@ const categories = [
 ]
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -59,4 +61,14 @@ describe('Nav component', () => {
 
         });
     })
+
+    it('renders', () => {
+        render(<Nav
+          categories={categories}
+          setCurrentCategory={mockSetCurrentCategory}
+          currentCategory={mockCurrentCategory}
+          contactSelected={mockContactSelected}
+          setContactSelected={mockSetContactSelected}
+        />);
+      })
 })
